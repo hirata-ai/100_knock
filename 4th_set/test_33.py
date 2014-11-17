60328e1e194834407043886545a5d6dae4e36d26
平田亜衣 <hirataai@sd.tmu.ac.jp>
2014-11-17 11:29:32 +0900

    modified test_33.py

--- 4th_set/test_33.py
+++ 4th_set/test_33.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python
 #-*-coding:utf-8-*-
 
-def load_dic(input_data,medline_txt):
+#echo "AA" |python test_33.py inflection_dump.txt   で実行
+
+def load_dic(input_data,word):
     import marshal
     inflection_dic = marshal.load(open(input_data))
-    for line in open(medline_txt):
-        line = line.strip()
-        words = line.split("\t")
-        for word in words:
-            if word in inflection_dic:
-                print word,inflection_dic[word]
+    #for line in open(medline_txt):
+    #    line = line.strip()
+    #    words = line.split("\t")
+    #    for word in words:
+    #        if word in inflection_dic:
+    #            print word,inflection_dic[word]
+    if word in inflection_dic:
+        print word,inflection_dic[word]
+    else:
+        print word
+        print "error"
 
 if __name__ == "__main__":
     import sys
-    load_dic(sys.argv[1],sys.argv[2])
+    for word in sys.stdin:
+        word = word.strip()
+        load_dic(sys.argv[1],word)
